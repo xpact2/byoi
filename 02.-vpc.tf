0d10d76e0cfa2a5112e4ce45aac77f9b09983997
Dimitar Batchvarov <d_batchvarov@yahoo.com>
2025-01-24 16:17:54 +0200

    coments

--- 02.-vpc.tf
+++ 02.-vpc.tf
@@ -1,182 +1,182 @@
-resource "aws_vpc" "byoi-vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# resource "aws_vpc" "byoi-vpc" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
 
-  tags = {
-    Name = "byoi-vpc"
-  }
-}
+#   tags = {
+#     Name = "byoi-vpc"
+#   }
+# }
 
-resource "aws_subnet" "byoi-public-subnet-1" {
-  vpc_id                  = aws_vpc.byoi-vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+# resource "aws_subnet" "byoi-public-subnet-1" {
+#   vpc_id                  = aws_vpc.byoi-vpc.id
+#   cidr_block              = "10.0.1.0/24"
+#   availability_zone       = "us-east-1a"
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "Public-Subnet-1"
-  }
-}
+#   tags = {
+#     Name = "Public-Subnet-1"
+#   }
+# }
 
-resource "aws_subnet" "byoi-public-subnet-2" {
-  vpc_id                  = aws_vpc.byoi-vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Public-Subnet-2"
-  }
-}
+# resource "aws_subnet" "byoi-public-subnet-2" {
+#   vpc_id                  = aws_vpc.byoi-vpc.id
+#   cidr_block              = "10.0.2.0/24"
+#   availability_zone       = "us-east-1b"
+#   map_public_ip_on_launch = true
+#   tags = {
+#     Name = "Public-Subnet-2"
+#   }
+# }
 
-resource "aws_subnet" "byoi-private-subnet-1" {
-  vpc_id            = aws_vpc.byoi-vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
+# resource "aws_subnet" "byoi-private-subnet-1" {
+#   vpc_id            = aws_vpc.byoi-vpc.id
+#   cidr_block        = "10.0.3.0/24"
+#   availability_zone = "us-east-1a"
 
-  tags = {
-    Name = "Private-Subnet-1"
-  }
-}
+#   tags = {
+#     Name = "Private-Subnet-1"
+#   }
+# }
 
-resource "aws_subnet" "byoi-private-subnet-2" {
-  vpc_id            = aws_vpc.byoi-vpc.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
+# resource "aws_subnet" "byoi-private-subnet-2" {
+#   vpc_id            = aws_vpc.byoi-vpc.id
+#   cidr_block        = "10.0.4.0/24"
+#   availability_zone = "us-east-1b"
 
-  tags = {
-    Name = "PrivateSubnet2"
-  }
-}
+#   tags = {
+#     Name = "PrivateSubnet2"
+#   }
+# }
 
-# 2 database subnets
-resource "aws_subnet" "byoi-database-subnet-1" {
-  vpc_id            = aws_vpc.byoi-vpc.id
-  cidr_block        = "10.0.5.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "database-subnet-1"
-  }
-}
+# # 2 database subnets
+# resource "aws_subnet" "byoi-database-subnet-1" {
+#   vpc_id            = aws_vpc.byoi-vpc.id
+#   cidr_block        = "10.0.5.0/24"
+#   availability_zone = "us-east-1a"
+#   tags = {
+#     Name = "database-subnet-1"
+#   }
+# }
 
-resource "aws_subnet" "byoi-database-subnet-2" {
-  vpc_id            = aws_vpc.byoi-vpc.id
-  cidr_block        = "10.0.6.0/24"
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "database-subnet-2"
-  }
-}
-
-
-
-# Internet Gateway
-resource "aws_internet_gateway" "byoi_igw" {
-  vpc_id = aws_vpc.byoi-vpc.id
-  tags = {
-    "Name" = "byoi-igw"
-  }
-}
-# Create Elastic IP for NAT Gateway
-resource "aws_eip" "nat_eip" {
-  vpc = true
-}
-
-# Nat Gateway
-resource "aws_nat_gateway" "byoi-nat-gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.byoi-public-subnet-1.id
-  tags = {
-    Name = "byoi-nat-gw"
-  }
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.byoi_igw]
-}
-
-######################################################
-# public route table
-# public subnets association into public route table
-# Add Internet Gateway into public route table
-######################################################
-# public route table
-resource "aws_route_table" "byoi-public-rt" {
-  vpc_id = aws_vpc.byoi-vpc.id
-  tags = {
-    "Name" = "public-rt"
-  }
-}
-
-# Associate both public subnets with public route table
-resource "aws_route_table_association" "public_subnet_association-1" {
-  route_table_id = aws_route_table.byoi-public-rt.id
-  subnet_id      = aws_subnet.byoi-public-subnet-1.id
-}
-
-resource "aws_route_table_association" "public_subnet_association-2" {
-  route_table_id = aws_route_table.byoi-public-rt.id
-  subnet_id      = aws_subnet.byoi-public-subnet-2.id
-}
-
-# Add Internet Gateway into public route table
-resource "aws_route" "byoi-route-igw" {
-  route_table_id         = aws_route_table.byoi-public-rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.byoi_igw.id
-}
+# resource "aws_subnet" "byoi-database-subnet-2" {
+#   vpc_id            = aws_vpc.byoi-vpc.id
+#   cidr_block        = "10.0.6.0/24"
+#   availability_zone = "us-east-1b"
+#   tags = {
+#     Name = "database-subnet-2"
+#   }
+# }
 
 
-######################################################
-# private route table
-# private subnets association into private route table
-# Add Nat Gateway into private route table
-######################################################
-# private route table
-resource "aws_route_table" "byoi-private-rt" {
-  vpc_id = aws_vpc.byoi-vpc.id
-  tags = {
-    "Name" = "private-rt"
-  }
-}
 
-# Associate both private subnets with private route table
-resource "aws_route_table_association" "private_subnet_association-1" {
-  route_table_id = aws_route_table.byoi-private-rt.id
-  subnet_id      = aws_subnet.byoi-private-subnet-1.id
-}
+# # Internet Gateway
+# resource "aws_internet_gateway" "byoi_igw" {
+#   vpc_id = aws_vpc.byoi-vpc.id
+#   tags = {
+#     "Name" = "byoi-igw"
+#   }
+# }
+# # Create Elastic IP for NAT Gateway
+# resource "aws_eip" "nat_eip" {
+#   vpc = true
+# }
 
-resource "aws_route_table_association" "private_subnet_association-2" {
-  route_table_id = aws_route_table.byoi-private-rt.id
-  subnet_id      = aws_subnet.byoi-private-subnet-2.id
-}
+# # Nat Gateway
+# resource "aws_nat_gateway" "byoi-nat-gw" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id     = aws_subnet.byoi-public-subnet-1.id
+#   tags = {
+#     Name = "byoi-nat-gw"
+#   }
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.byoi_igw]
+# }
 
-# Add Nat Gateway into private route table
-resource "aws_route" "byoi-route-nat-gw" {
-  route_table_id         = aws_route_table.byoi-private-rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.byoi-nat-gw.id
-}
+# ######################################################
+# # public route table
+# # public subnets association into public route table
+# # Add Internet Gateway into public route table
+# ######################################################
+# # public route table
+# resource "aws_route_table" "byoi-public-rt" {
+#   vpc_id = aws_vpc.byoi-vpc.id
+#   tags = {
+#     "Name" = "public-rt"
+#   }
+# }
+
+# # Associate both public subnets with public route table
+# resource "aws_route_table_association" "public_subnet_association-1" {
+#   route_table_id = aws_route_table.byoi-public-rt.id
+#   subnet_id      = aws_subnet.byoi-public-subnet-1.id
+# }
+
+# resource "aws_route_table_association" "public_subnet_association-2" {
+#   route_table_id = aws_route_table.byoi-public-rt.id
+#   subnet_id      = aws_subnet.byoi-public-subnet-2.id
+# }
+
+# # Add Internet Gateway into public route table
+# resource "aws_route" "byoi-route-igw" {
+#   route_table_id         = aws_route_table.byoi-public-rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.byoi_igw.id
+# }
 
 
-######################################################
-# database route table
-# database subnets association into database route table
-######################################################
-# database route table
-resource "aws_route_table" "byoi-database-rt" {
-  vpc_id = aws_vpc.byoi-vpc.id
-  tags = {
-    "Name" = "database-rt"
-  }
-}
+# ######################################################
+# # private route table
+# # private subnets association into private route table
+# # Add Nat Gateway into private route table
+# ######################################################
+# # private route table
+# resource "aws_route_table" "byoi-private-rt" {
+#   vpc_id = aws_vpc.byoi-vpc.id
+#   tags = {
+#     "Name" = "private-rt"
+#   }
+# }
 
-# Associate both database subnets with database route table
-resource "aws_route_table_association" "database_subnet_association-1" {
-  route_table_id = aws_route_table.byoi-database-rt.id
-  subnet_id      = aws_subnet.byoi-database-subnet-1.id
-}
+# # Associate both private subnets with private route table
+# resource "aws_route_table_association" "private_subnet_association-1" {
+#   route_table_id = aws_route_table.byoi-private-rt.id
+#   subnet_id      = aws_subnet.byoi-private-subnet-1.id
+# }
 
-resource "aws_route_table_association" "database_subnet_association-2" {
-  route_table_id = aws_route_table.byoi-database-rt.id
-  subnet_id      = aws_subnet.byoi-database-subnet-2.id
-}
+# resource "aws_route_table_association" "private_subnet_association-2" {
+#   route_table_id = aws_route_table.byoi-private-rt.id
+#   subnet_id      = aws_subnet.byoi-private-subnet-2.id
+# }
+
+# # Add Nat Gateway into private route table
+# resource "aws_route" "byoi-route-nat-gw" {
+#   route_table_id         = aws_route_table.byoi-private-rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.byoi-nat-gw.id
+# }
+
+
+# ######################################################
+# # database route table
+# # database subnets association into database route table
+# ######################################################
+# # database route table
+# resource "aws_route_table" "byoi-database-rt" {
+#   vpc_id = aws_vpc.byoi-vpc.id
+#   tags = {
+#     "Name" = "database-rt"
+#   }
+# }
+
+# # Associate both database subnets with database route table
+# resource "aws_route_table_association" "database_subnet_association-1" {
+#   route_table_id = aws_route_table.byoi-database-rt.id
+#   subnet_id      = aws_subnet.byoi-database-subnet-1.id
+# }
+
+# resource "aws_route_table_association" "database_subnet_association-2" {
+#   route_table_id = aws_route_table.byoi-database-rt.id
+#   subnet_id      = aws_subnet.byoi-database-subnet-2.id
+# }
--- 06-alb.tf
+++ 06-alb.tf
@@ -1,62 +1,62 @@
-resource "aws_lb_target_group" "byoi_tg" {
-  target_type = "instance"
-  name        = "byoi-target-group"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.byoi-vpc.id
+# resource "aws_lb_target_group" "byoi_tg" {
+#   target_type = "instance"
+#   name        = "byoi-target-group"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = aws_vpc.byoi-vpc.id
 
-  stickiness { #here no need to enabled this stickiness argument,#
-    #enabled = true
-    enabled         = false
-    type            = "lb_cookie"
-    cookie_duration = 3600
-  }
+#   stickiness { #here no need to enabled this stickiness argument,#
+#     #enabled = true
+#     enabled         = false
+#     type            = "lb_cookie"
+#     cookie_duration = 3600
+#   }
 
-  health_check {
-    path                = "/"
-    healthy_threshold   = 3
-    unhealthy_threshold = 2
-    timeout             = 5
-    interval            = 5
-  }
+#   health_check {
+#     path                = "/"
+#     healthy_threshold   = 3
+#     unhealthy_threshold = 2
+#     timeout             = 5
+#     interval            = 5
+#   }
 
-}
+# }
 
-# aws_lb_target_group_attachment
-resource "aws_lb_target_group_attachment" "instances-attachment-1" {
-  target_group_arn = aws_lb_target_group.byoi_tg.arn
-  target_id        = aws_instance.byoi-app-server-1.id
-  port             = 80
-}
+# # aws_lb_target_group_attachment
+# resource "aws_lb_target_group_attachment" "instances-attachment-1" {
+#   target_group_arn = aws_lb_target_group.byoi_tg.arn
+#   target_id        = aws_instance.byoi-app-server-1.id
+#   port             = 80
+# }
 
-resource "aws_lb_target_group_attachment" "instances-attachment-2" {
-  target_group_arn = aws_lb_target_group.byoi_tg.arn
-  target_id        = aws_instance.byoi-app-server-2.id
-  port             = 80
-}
-
-
-# ALP
-resource "aws_lb" "byoi_alb" {
-  name                       = "byoi-alb"
-  internal                   = false
-  load_balancer_type         = "application"
-  subnets                    = [aws_subnet.byoi-public-subnet-1.id, aws_subnet.byoi-public-subnet-2.id]
-  security_groups            = [aws_security_group.byoi-ALB-SG.id]
-  enable_deletion_protection = false
-  /* "enable_deletion_protection" If true, deletion of the load balancer will be disabled via
-  the AWS API. This will prevent Terraform from deleting the load balancer.*/
-
-}
+# resource "aws_lb_target_group_attachment" "instances-attachment-2" {
+#   target_group_arn = aws_lb_target_group.byoi_tg.arn
+#   target_id        = aws_instance.byoi-app-server-2.id
+#   port             = 80
+# }
 
 
-# Load Balancer Listener on port 80
-resource "aws_lb_listener" "alb_forward_listener_80" {
-  load_balancer_arn = aws_lb.byoi_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.byoi_tg.arn
-  }
-}
+# # ALP
+# resource "aws_lb" "byoi_alb" {
+#   name                       = "byoi-alb"
+#   internal                   = false
+#   load_balancer_type         = "application"
+#   subnets                    = [aws_subnet.byoi-public-subnet-1.id, aws_subnet.byoi-public-subnet-2.id]
+#   security_groups            = [aws_security_group.byoi-ALB-SG.id]
+#   enable_deletion_protection = false
+#   /* "enable_deletion_protection" If true, deletion of the load balancer will be disabled via
+#   the AWS API. This will prevent Terraform from deleting the load balancer.*/
+
+# }
+
+
+# # Load Balancer Listener on port 80
+# resource "aws_lb_listener" "alb_forward_listener_80" {
+#   load_balancer_arn = aws_lb.byoi_alb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.byoi_tg.arn
+#   }
+# }
